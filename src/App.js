3dc7f2eecfebebb7c088e9bb8f8f105faadabc35
Adriel <adriel.fsantos@outlook.com>
2021-12-19 16:46:41 -0300

    Implementa pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import BarraSocial from "./components/BarraSocial";
 import Section from "./components/Section";
+import imgHome from "./img/home.svg";
 
 function App() {
   return (
     <Section id="home">
-      <BarraSocial />
+      <div>
+        <div>
+        <h1>Olá, meu nome é Adriel</h1>
+        <p>Sou universitário e aspirante a desenvolvedor web</p>
+        </div>
+        <BarraSocial />
+      </div>
+      <div>
+        <img src={imgHome} alt="" />
+      </div>
     </Section>
   );
 }
