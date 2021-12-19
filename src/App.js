b1aefbf6d93afe4ea764a8bf774836c3f62e2c79
Adriel <adriel.fariasan@gmail.com>
2021-12-19 20:10:18 -0300

    Separa Home de App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import "./App.css";
-import BarraSocial from "./components/BarraSocial";
-import Section from "./components/Section";
-import imgHome from "./img/home.svg";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <Section id="home" colReverse={false}>
-      <div>
-        <div>
-          <h1>Olá, meu nome é Adriel</h1>
-          <p>Sou universitário e aspirante a desenvolvedor web</p>
-        </div>
-        <BarraSocial />
-      </div>
-      <div>
-        <img src={imgHome} alt="" />
-      </div>
-    </Section>
-  );
+  return <Home />;
 }
 
 export default App;
