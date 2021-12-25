24a97e1b235b34cfa54811e6d8ce07be37c9aa39
Adriel <adriel.fariasan@gmail.com>
2021-12-25 18:04:33 -0300

    Adiciona componente Nav

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import Habilidades from "./components/Habilidades";
 import Experiencias from "./components/Experiencias";
 import Portfolio from "./components/Portfolio.js";
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { useEffect } from "react";
 
 function App() {
+  const secAlt = [];
+  useEffect(() => {
+    const sections = document.getElementsByTagName("section");
+
+    for (let i = 0; i < sections.length; i++) {
+      secAlt.push({
+        id: sections[i].id,
+        altura: sections[i].offsetHeight,
+      });
+    }
+  });
   return (
     <>
+      <Nav secAlt={secAlt} />
       <Home />
       <Formacao />
       <Habilidades />
