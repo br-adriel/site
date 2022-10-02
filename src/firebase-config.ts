5a460c2ae8d3c6928f25be9abafb4348794fda63
Adriel <adriel.fsantos@outlook.com>
2022-10-01 22:34:47 -0300

    Configura conexao com firebase

---
+++ src/firebase-config.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: `${process.env.apiKey}`,
+  authDomain: `${process.env.authDomain}`,
+  projectId: `${process.env.projectId}`,
+  storageBucket: `${process.env.storageBucket}`,
+  messagingSenderId: `${process.env.messagingSenderId}`,
+  appId: `${process.env.appId}`,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
