0a070231cd1a5304b674e47813f62197a6911c91
Adriel <adriel.fsantos@outlook.com>
2022-10-12 13:37:19 -0300

    Adiciona animação em headings de HelloSection

--- src/components/HelloSection/HelloSection.tsx
+++ src/components/HelloSection/HelloSection.tsx
@@ -1,7 +1,8 @@
+import { motion } from 'framer-motion';
 import { At, Github, Linkedin } from 'react-bootstrap-icons';
+import desktop_img from '../../assets/img/home_desktop.svg';
 import SocialBar from '../SocialBar';
 import * as S from './HelloSection.style';
-import desktop_img from '../../assets/img/home_desktop.svg';
 
 const HelloSection = () => {
   const socialItems = [
@@ -28,8 +29,20 @@ const HelloSection = () => {
   return (
     <S.Section>
       <div>
-        <h1>Adriel Santos</h1>
-        <h2>Desenvolvedor Frontend</h2>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Adriel Santos
+        </motion.h1>
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          Desenvolvedor Frontend
+        </motion.h2>
         <SocialBar socialItems={socialItems} />
       </div>
       <img src={desktop_img} alt='' />
