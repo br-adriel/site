49147a32c59b8b7de08fe8689cb884b31cbdeb5d
Adriel <adriel.fsantos@outlook.com>
2022-10-15 22:17:53 -0300

    Implementa carregamento de habilidades

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,11 +1,44 @@
-import * as S from './SkillsSection.styled';
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import techImg from '../../assets/img/tech_bg.svg';
+import { db } from '../../firebase-config';
+import SkillCard from './SkillCard/SkillCard';
+import * as S from './SkillsSection.styled';
+
+interface ISkill {
+  id: string;
+  imagem: string;
+  nome: string;
+  descricao: string;
+}
 
 const SkillsSection = () => {
+  const [skills, setSkills]: [ISkill[], any] = useState([]);
+  const skillsCollectionRef = collection(db, 'habilidades');
+
+  useEffect(() => {
+    const getHabilidades = async () => {
+      const data = await getDocs(skillsCollectionRef);
+      setSkills(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getHabilidades();
+  }, []);
+
   return (
     <S.Section>
       <S.ContentDiv>
         <h2>Habilidades e tecnologias</h2>
+        <S.SkillGrid>
+          {skills.map((skill) => (
+            <SkillCard
+              key={skill.id}
+              icon={skill.imagem}
+              skill={skill.nome}
+              description={skill.descricao}
+            />
+          ))}
+        </S.SkillGrid>
       </S.ContentDiv>
       <img src={techImg} alt='' />
     </S.Section>
