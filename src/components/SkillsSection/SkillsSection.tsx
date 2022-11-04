baa5170dbcaf84fb82978445538cbd524dec00bf
Adriel <adriel.fsantos@outlook.com>
2022-11-04 20:27:09 -0300

    Insere Loading em SkillsSection

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -3,17 +3,20 @@ import { useEffect, useState } from 'react';
 import techImg from '../../assets/img/tech_bg.svg';
 import { db } from '../../firebase-config';
 import { ISkill } from '../../global/types';
+import Loading from '../Loading';
 import SkillCard from './SkillCard/SkillCard';
 import * as S from './SkillsSection.styled';
 
 const SkillsSection = () => {
   const [skills, setSkills]: [ISkill[], any] = useState([]);
+  const [loading, setLoading] = useState(true);
   const skillsCollectionRef = collection(db, 'habilidades');
 
   useEffect(() => {
     const getHabilidades = async () => {
       const data = await getDocs(skillsCollectionRef);
       setSkills(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setLoading(false);
     };
 
     getHabilidades();
@@ -21,19 +24,23 @@ const SkillsSection = () => {
 
   return (
     <S.Section>
-      <S.ContentDiv>
-        <h2>Habilidades e tecnologias</h2>
-        <S.SkillGrid>
-          {skills.map((skill) => (
-            <SkillCard
-              key={skill.id}
-              icon={skill.imagem}
-              skill={skill.nome}
-              description={skill.descricao}
-            />
-          ))}
-        </S.SkillGrid>
-      </S.ContentDiv>
+      {loading ? (
+        <Loading />
+      ) : (
+        <S.ContentDiv>
+          <h2>Habilidades e tecnologias</h2>
+          <S.SkillGrid>
+            {skills.map((skill) => (
+              <SkillCard
+                key={skill.id}
+                icon={skill.imagem}
+                skill={skill.nome}
+                description={skill.descricao}
+              />
+            ))}
+          </S.SkillGrid>
+        </S.ContentDiv>
+      )}
       <img src={techImg} alt='' />
     </S.Section>
   );
