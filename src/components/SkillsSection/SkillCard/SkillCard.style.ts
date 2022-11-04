c804554aa5455de646aa32f0567d89a8e3f3dfa2
Adriel <adriel.fsantos@outlook.com>
2022-11-04 00:24:24 -0300

    Move css de Card para componente próprio

--- src/components/SkillsSection/SkillCard/SkillCard.style.ts
+++ src/components/SkillsSection/SkillCard/SkillCard.style.ts
@@ -1,27 +1,12 @@
-import { motion } from 'framer-motion';
 import styled from 'styled-components';
-import { ITheme } from '../../ThemeSwitch/Themes';
+import DefaultCard from '../../Card';
 
-export const Card = styled(motion.div)`
-  background: ${({ theme }: { theme: ITheme }) => theme.bg2};
-  padding: 12px;
-  border-radius: 5px;
-  display: flex;
-  gap: 12px;
-  align-items: flex-start;
-  box-shadow: 0 0 2px rgba(200, 200, 200, 0.1);
-  transition: all 0.4s ease-in-out;
-  position: relative;
-
+export const Card = styled(DefaultCard)`
   img {
     width: 20%;
     background: #fff;
     padding: 5px;
     border-radius: 5px;
-  }
-
-  &:hover {
-    box-shadow: 0 2px 6px rgba(200, 200, 200, 0.2);
   }
 `;
 
