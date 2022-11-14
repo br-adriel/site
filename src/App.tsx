7e1e1ea3d80a7be9898ebe6520b63c90842296bb
Adriel <adriel.fsantos@outlook.com>
2022-11-14 19:08:56 -0300

    Remove import de Home em App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import Home from './pages/Home';
-import GlobalStyle from './components/GlobalStyle';
-import { ThemeProvider } from 'styled-components';
 import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './components/GlobalStyle';
 import { dark, light } from './components/ThemeSwitch/Themes';
 import ThemeSwitch from './components/ThemeSwitch/ThemeSwitch';
-import { Outlet } from 'react-router-dom';
 
 function App() {
   const [useDarkTheme, setUseDarkTheme] = useState(false);
-
   const setTheme = () => setUseDarkTheme((prev) => !prev);
 
   return (
