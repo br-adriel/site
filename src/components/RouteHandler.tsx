339922e472634f4f01697e601885b63c4a6be0b4
Adriel <adriel.fsantos@outlook.com>
2022-11-14 22:51:48 -0300

    Implemeta rota para filtrar projetos

--- src/components/RouteHandler.tsx
+++ src/components/RouteHandler.tsx
@@ -4,6 +4,7 @@ import Error from '../pages/Error';
 import Home from '../pages/Home';
 import Project from '../pages/Project';
 import Projects from '../pages/Projects';
+import Filter from '../pages/Filter';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: '',
         element: <Home />,
+      },
+      {
+        path: 'projetos/filtrar/:techName',
+        element: <Filter />,
       },
       {
         path: 'projetos/:projectId',
