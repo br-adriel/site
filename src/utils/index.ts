bbba4fc7ca334a0d433c66f85c44742800cca333
Adriel <adriel.fsantos@outlook.com>
2022-11-16 10:55:44 -0300

    Atualiza exportacoes da pasta utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,13 @@
 import getMonthName from './getMonthName';
 import getProject from './getProject';
 import getQuery from './getQuery';
+import getUserThemePreference from './getUserThemePreference';
+import setUserThemePreference from './serUserThemePreference';
 
-export { getMonthName, getProject, getQuery };
+export {
+  getMonthName,
+  getProject,
+  getQuery,
+  getUserThemePreference,
+  setUserThemePreference,
+};
