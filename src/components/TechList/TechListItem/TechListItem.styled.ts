85753277288c834f2aecdb41311b0f5612a9be60
Adriel <adriel.fsantos@outlook.com>
2022-11-16 00:31:12 -0300

    Melhora visual de TechListItem

--- src/components/TechList/TechListItem/TechListItem.styled.ts
+++ src/components/TechList/TechListItem/TechListItem.styled.ts
@@ -2,11 +2,15 @@ import styled from 'styled-components';
 import { ITheme } from '../../ThemeSwitch/Themes';
 
 export const ListItem = styled.li`
+  flex-grow: 1;
+  display: flex;
   border-radius: 5px;
   background: ${({ theme }: { theme: ITheme }) => theme.bg};
   transition: all 0.3 ease;
   list-style: none;
   color: ${({ theme }: { theme: ITheme }) => theme.color};
+  justify-content: center;
+  align-items: center;
 
   &:hover {
     background: ${({ theme }: { theme: ITheme }) => theme.primary};
