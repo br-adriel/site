320891a404b9ed949c4b4b64a278697a4b15de29
Adriel <adriel.fsantos@outlook.com>
2022-11-16 10:16:35 -0300

    Ajusta tamanho da imagem de SkillsSection

--- src/components/SkillsSection/SkillsSection.styled.ts
+++ src/components/SkillsSection/SkillsSection.styled.ts
@@ -6,17 +6,23 @@ export const Section = styled(ContainerSection)`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  gap: 12px;
+  gap: 20px;
 
   & > img {
-    max-height: max(90vh, 90%);
+    max-height: max(70vh, 70%);
     display: none;
-    width: 30%;
+    width: 25%;
   }
 
   @media screen and (min-width: 992px) {
     & > img {
       display: block;
+    }
+  }
+
+  @media screen and (min-width: 992px) {
+    & > img {
+      width: 20%;
     }
   }
 `;
@@ -37,7 +43,7 @@ export const SkillGrid = styled.div`
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media screen and (min-width: 992px) {
+  @media screen and (min-width: 1200px) {
     grid-template-columns: repeat(3, 1fr);
   }
 `;
