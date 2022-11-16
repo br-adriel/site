0c9fc64d970d42150bff9f772f78775b0fc183c4
Adriel <adriel.fsantos@outlook.com>
2022-11-15 23:44:46 -0300

    Torna HelloSection responsiva

--- src/components/HelloSection/HelloSection.style.ts
+++ src/components/HelloSection/HelloSection.style.ts
@@ -3,16 +3,53 @@ import ContainerSection from '../ContainerSection';
 
 export const Section = styled(ContainerSection)`
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  gap: 40px;
+  gap: 20px;
 
   img {
     max-height: 70vh;
   }
 
+  h1 {
+    font-size: 3rem;
+  }
+
+  h1,
+  h2 {
+    text-align: center;
+  }
+
   h2 {
     margin-top: -0.75rem;
     margin-bottom: 10px;
+  }
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-around;
+    gap: 40px;
+
+    h1 {
+      font-size: 4rem;
+    }
+
+    h1,
+    h2 {
+      text-align: left;
+    }
+  }
+
+  @media screen and (min-width: 992px) {
+    h1 {
+      font-size: 4.5rem;
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    h1 {
+      font-size: 5rem;
+    }
   }
 `;
