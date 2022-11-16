7d9188ef9e277f4e7fe000e0eb1e40056c991bd8
Adriel <adriel.fsantos@outlook.com>
2022-11-16 10:56:21 -0300

    Implementa persistencia de tema escolhido

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './components/GlobalStyle';
 import { dark, light } from './components/ThemeSwitch/Themes';
 import ThemeSwitch from './components/ThemeSwitch/ThemeSwitch';
+import { getUserThemePreference, setUserThemePreference } from './utils';
 
 function App() {
   const [useDarkTheme, setUseDarkTheme] = useState(false);
-  const setTheme = () => setUseDarkTheme((prev) => !prev);
+  const setTheme = () => {
+    setUseDarkTheme((prev) => {
+      setUserThemePreference(!prev ? 'dark' : 'light');
+      return !prev;
+    });
+  };
+
+  useEffect(() => {
+    const theme = getUserThemePreference();
+    if (theme === 'dark') setUseDarkTheme(true);
+  }, []);
 
   return (
     <ThemeProvider theme={useDarkTheme ? dark : light}>
