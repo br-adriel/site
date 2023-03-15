32ad330a7f919e6a7b6bb2dcff84a155dffdd006
Adriel <adriel.fsantos@outlook.com>
2023-03-15 18:25:53 -0300

    Adiciona propriedades de sombra ao tema

--- v2/src/components/ThemeSwitch/Themes.ts
+++ v2/src/components/ThemeSwitch/Themes.ts
@@ -5,6 +5,8 @@ export interface ITheme {
   bg2: string;
   color: string;
   colorAlt: string;
+  shadowSm: string;
+  shadowMd: string;
 }
 
 export const lightTheme: ITheme = {
@@ -14,13 +16,17 @@ export const lightTheme: ITheme = {
   bg2: '#ffffff',
   color: '#000000',
   colorAlt: '#ffffff',
+  shadowSm: '0 0 2px rgba(200, 200, 200, 0.1)',
+  shadowMd: '0 0 2px rgba(200, 200, 200, 0.2)',
 };
 
 export const darkTheme: ITheme = {
   primary: '#3a86ff',
   secondary: '#8338ec',
   bg: '#1f1f1f',
-  bg2: '#111111',
+  bg2: '#1A1A1A',
   color: '#ffffff',
   colorAlt: '#000000',
+  shadowSm: '0 0 2px rgba(10, 10, 10, 0.1)',
+  shadowMd: '0 0 2px rgba(10, 10, 10, 0.2)',
 };
