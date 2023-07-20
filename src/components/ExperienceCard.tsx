691ce6bd673785e8224c6ca54bcbb4a18f71005b
Adriel <adriel.fsantos@outlook.com>
2023-07-20 18:26:11 -0300

    Altera sombra de ExperienceCard

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -14,7 +14,7 @@ export default function ExperienceCard({
   tarefas,
 }: IProps) {
   return (
-    <div className='bg-alt_bg shadow-sm rounded py-3 px-4 hover:shadow transition-shadow'>
+    <div className='bg-alt_bg shadow rounded py-3 px-4 hover:shadow-md transition-shadow'>
       <h3 className='text-2xl mb-1 font-medium'>{empresa}</h3>
       <p className='text-md opacity-70'>{data}</p>
       <hr className='border opacity-10 my-2 rounded' />
