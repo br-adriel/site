df273da97635f850ac6420a53e61597518cb201f
Adriel <adriel.fsantos@outlook.com>
2023-07-20 16:16:00 -0300

    Insere ExperienceSection em página Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ExperienceSection from '@/components/home/ExperienceSection';
 import HelloSection from '@/components/home/HelloSection';
 
 export default function Home() {
   return (
     <main className='flex flex-col'>
       <HelloSection />
+      <ExperienceSection />
     </main>
   );
 }
