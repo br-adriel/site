2a266c202a51ed0de87be385024863cdcd3f0abb
Adriel <adriel.fsantos@outlook.com>
2023-07-19 21:11:35 -0300

    Anima componente SocialLink

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,4 +1,13 @@
+'use client';
+
 import { Icon } from 'react-bootstrap-icons';
+import { motion } from 'framer-motion';
+
+const linkAnimation = {
+  initial: { scale: 0.4, opacity: 0 },
+  animate: { scale: 1, opacity: 1 },
+  transition: { duration: 0.5 },
+};
 
 interface IProps {
   Icon: Icon;
@@ -8,14 +17,17 @@ interface IProps {
 
 export default function SocialLink({ Icon, link, title }: IProps) {
   return (
-    <a
+    <motion.a
+      {...linkAnimation}
       href={link}
       target='_blank'
       rel='noopener noreferrer'
       title={title}
-      className='text-3xl bg-blue-600 p-3 rounded-full hover:bg-violet-700'
+      className='text-3xl rounded-full'
     >
-      {<Icon />}
-    </a>
+      <div className='hover:bg-violet-700 bg-blue-600 p-3 transition-all hover:scale-95 rounded-full'>
+        {<Icon />}
+      </div>
+    </motion.a>
   );
 }
