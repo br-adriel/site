607e3ebcb28e4fbd34242b4fa9d70d01cdf7cb81
Adriel <adriel.fsantos@outlook.com>
2023-07-20 16:14:55 -0300

    Adiciona cor de fundo alternativa

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        alt_bg: {
+          DEFAULT: 'rgb(var(--background-rgb-alt))',
+        },
       },
     },
   },
   plugins: [],
-}
+};
