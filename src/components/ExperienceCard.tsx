878191b01066c11fe2c545266eca23cff0873638
Adriel <adriel.fsantos@outlook.com>
2023-07-20 16:15:34 -0300

    Adiciona componente ExperienceCard

---
+++ src/components/ExperienceCard.tsx
@@ -0,0 +1,34 @@
+import { ArrowRight } from 'react-bootstrap-icons';
+
+interface IProps {
+  empresa: string;
+  data: string;
+  cargo: string;
+  tarefas: string[];
+}
+
+export default function ExperienceCard({
+  cargo,
+  data,
+  empresa,
+  tarefas,
+}: IProps) {
+  return (
+    <div className='bg-alt_bg shadow-sm rounded py-3 px-4 hover:shadow transition-shadow'>
+      <h3 className='text-2xl mb-1 font-medium'>{empresa}</h3>
+      <p className='text-md opacity-70'>{data}</p>
+      <hr className='border opacity-10 my-2 rounded' />
+      <h4 className='text-xl font-medium'>{cargo}</h4>
+      <ul>
+        {tarefas.map((tarefa) => {
+          return (
+            <li className='flex items-center' key={tarefa}>
+              <ArrowRight className='inline-block mr-2 opacity-60' />
+              <span>{tarefa}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
