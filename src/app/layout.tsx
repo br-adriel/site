6c079036cd1e4f4a06cd45c0660b406068620bde
Adriel <adriel.fsantos@outlook.com>
2023-07-19 21:38:06 -0300

    Adiciona versões alternativas de favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,37 @@ export const metadata: Metadata = {
   title: 'Adriel Santos - Desenvolvedor Fullstack',
   description: 'Conheça meu trabalho como desenvolvedor web',
   themeColor: '#1d4ed8',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/icons/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/icons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      url: '/icons/android-chrome-192x192.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '512x512',
+      url: '/icons/android-chrome-512x512.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/icons/apple-touch-icon.png',
+    },
+  ],
 };
 
 export default function RootLayout({
