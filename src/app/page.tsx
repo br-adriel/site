c53f0074a955186d5910ea3148fda82c6dec0d30
Adriel <adriel.fsantos@outlook.com>
2023-07-20 18:26:57 -0300

    Insere SkillsSection em página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import ExperienceSection from '@/components/home/ExperienceSection';
 import HelloSection from '@/components/home/HelloSection';
+import SkillsSection from '@/components/home/SkillsSection';
 
 export default function Home() {
   return (
     <main className='flex flex-col'>
       <HelloSection />
       <ExperienceSection />
+      <SkillsSection />
     </main>
   );
 }
