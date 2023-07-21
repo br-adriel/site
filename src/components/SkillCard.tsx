9226c794cc24df3f7b767245714db2611c67df61
Adriel <adriel.fsantos@outlook.com>
2023-07-21 00:38:02 -0300

    Anima cards de habilidade

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,19 +1,31 @@
+'use client';
+
+import { fadeInGrow } from '@/animations/FadeInGrow';
 import { Trigger } from '@radix-ui/react-dialog';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 
-export default function SkillCard() {
+interface IProps {
+  delay?: number;
+}
+
+export default function SkillCard({ delay }: IProps) {
+  const cardAnimation = fadeInGrow(0.1 * (delay || 0), 0.2);
+
   return (
-    <Trigger
+    <motion.div
+      {...cardAnimation}
       className='bg-alt_bg p-2 rounded flex items-center justify-center cursor-pointer shadow hover:shadow-md transition-shadow'
-      title='HTML'
     >
-      <Image
-        src='https://www.svgrepo.com/show/349402/html5.svg'
-        alt=''
-        width={80}
-        height={80}
-        className='w-full p-2 bg-white rounded'
-      />
-    </Trigger>
+      <Trigger title='HTML'>
+        <Image
+          src='https://www.svgrepo.com/show/349402/html5.svg'
+          alt=''
+          width={80}
+          height={80}
+          className='w-full p-2 bg-white rounded'
+        />
+      </Trigger>
+    </motion.div>
   );
 }
