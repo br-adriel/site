00771078d68a93954bb5f7ae181d24baeb840ee4
Adriel <adriel.fsantos@outlook.com>
2023-07-21 20:27:20 -0300

    Insere ProjectSection em Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import EducationSection from '@/components/home/EducationSection';
 import ExperienceSection from '@/components/home/ExperienceSection';
 import HelloSection from '@/components/home/HelloSection';
+import ProjectsSection from '@/components/home/ProjectsSection';
 import SkillsSection from '@/components/home/SkillsSection';
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <ExperienceSection />
       <SkillsSection />
       <EducationSection />
+      <ProjectsSection />
     </main>
   );
 }
