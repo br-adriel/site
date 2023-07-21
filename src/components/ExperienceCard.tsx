e214254e64c1770171e0ec7b4e81d3702b816dc8
Adriel <adriel.fsantos@outlook.com>
2023-07-20 23:36:26 -0300

    Anima ExperienceCard

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,20 +1,31 @@
+'use client';
+
+import { fadeInUpAnimation } from '@/animations/FadeInUp';
+import { delay, motion } from 'framer-motion';
 import { ArrowRight } from 'react-bootstrap-icons';
 
 interface IProps {
-  empresa: string;
-  data: string;
   cargo: string;
+  data: string;
+  delay?: number;
+  empresa: string;
   tarefas: string[];
 }
 
 export default function ExperienceCard({
   cargo,
   data,
+  delay,
   empresa,
   tarefas,
 }: IProps) {
+  const animation = fadeInUpAnimation(0.2 * (delay || 0));
+
   return (
-    <div className='bg-alt_bg shadow rounded py-3 px-4 hover:shadow-md transition-shadow'>
+    <motion.div
+      {...animation}
+      className='bg-alt_bg shadow rounded py-3 px-4 hover:shadow-md transition-shadow'
+    >
       <h3 className='text-2xl mb-1 font-medium'>{empresa}</h3>
       <p className='text-md opacity-70'>{data}</p>
       <hr className='border opacity-10 my-2 rounded' />
@@ -29,6 +40,6 @@ export default function ExperienceCard({
           );
         })}
       </ul>
-    </div>
+    </motion.div>
   );
 }
