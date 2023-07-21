ec5ec452726c8303527dcbe75a5224f0d82ac09c
Adriel <adriel.fsantos@outlook.com>
2023-07-21 20:25:57 -0300

    Altera tamanho do titulo em ProjectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,10 +11,10 @@ export default function ProjectCard() {
         alt=''
         width={1067}
         height={600}
-        className='w-full rounded mb-1'
+        className='w-full rounded mb-2'
         src='https://adrielfsantos.vercel.app/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fportifolio-32f96.appspot.com%2Fo%2Fprojects%252Fexpress-members-only.png%3Falt%3Dmedia%26token%3D312995d1-e0bd-40c5-8c81-b73a592da76a&w=3840&q=75'
       />
-      <h3 className='text-lg'>Nome do projeto</h3>
+      <h3 className='text-2xl'>Nome do projeto</h3>
     </Link>
   );
 }
