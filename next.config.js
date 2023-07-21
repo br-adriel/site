99cbd90fd988802fc6675e375e49b04b4aaf389d
Adriel <adriel.fsantos@outlook.com>
2023-07-20 22:51:46 -0300

    Configura host de imagens

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'adrielfsantos.vercel.app',
+        port: '',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
