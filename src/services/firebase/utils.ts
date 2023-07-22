acf7b447ef846fff271b521b4fe3c972503b39b8
Adriel <adriel.fsantos@outlook.com>
2023-07-22 18:25:06 -0300

    Adiciona função getQuery

---
+++ src/services/firebase/utils.ts
@@ -0,0 +1,10 @@
+import { DocumentData, Query, getDocs } from 'firebase/firestore';
+
+export async function getQuery(query: Query<DocumentData>) {
+  const data = await getDocs(query);
+  const fetchedObjects = data.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return fetchedObjects;
+}
