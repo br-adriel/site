be0bf975273fc18e4de59232695435f64e97f3ad
Adriel <adriel.fsantos@outlook.com>
2023-07-21 21:08:29 -0300

    Remove animação não performada de ModalBackground

--- src/components/Modal/ModalBackground.tsx
+++ src/components/Modal/ModalBackground.tsx
@@ -1,17 +1,7 @@
-'use client';
-
-import { fadeInAnimation } from '@/animations/FadeIn';
-import { motion } from 'framer-motion';
-
 export default function ModalBackground() {
-  const modalBgAnimation = fadeInAnimation(0.2);
-
   return (
-    <motion.div
-      {...modalBgAnimation}
-      className='fixed top-0 left-0 w-screen h-screen z-0'
-    >
+    <div className='fixed top-0 left-0 w-screen h-screen z-0'>
       <div className='w-full h-full bg-blue-950 opacity-20' />
-    </motion.div>
+    </div>
   );
 }
