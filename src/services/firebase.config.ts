b1b6c3c3d19078e659d982fd50930055a5155fbf
Adriel <adriel.fsantos@outlook.com>
2023-07-22 17:26:31 -0300

    Configura firebase

---
+++ src/services/firebase.config.ts
@@ -0,0 +1,15 @@
+import { getFirestore } from '@firebase/firestore';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
