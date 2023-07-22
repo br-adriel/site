e291b6683d51a39e4971640f1e61b17f648ae3f8
Adriel <adriel.fsantos@outlook.com>
2023-07-22 18:25:20 -0300

    Adiciona interface de Experiencia

---
+++ src/interfaces/IExperience.ts
@@ -0,0 +1,9 @@
+export default interface IExperience {
+  ano_inicio: number;
+  cargo: string;
+  empresa: string;
+  id: string;
+  mes_inicio: number;
+  ano_fim?: number;
+  mes_fim?: number;
+}
