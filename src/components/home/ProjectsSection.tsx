c2a947d3d404fd5ccdb03bc406b7ed4832fc5202
Adriel <adriel.fsantos@outlook.com>
2023-07-21 21:05:22 -0300

    Atualiza botão de ProjectsSection

--- src/components/home/ProjectsSection.tsx
+++ src/components/home/ProjectsSection.tsx
@@ -2,7 +2,7 @@
 
 import { fadeInUpAnimation } from '@/animations/FadeInUp';
 import { motion } from 'framer-motion';
-import Link from 'next/link';
+import LinkButton from '../LinkButton';
 import ProjectCard from '../ProjectCard';
 
 export default function ProjectsSection() {
@@ -12,12 +12,7 @@ export default function ProjectsSection() {
     <section className='container mx-auto min-h-screen flex flex-col justify-center p-5 gap-2'>
       <div className='flex justify-between gap-5 items-center'>
         <h2 className='text-4xl font-semibold mb-3'>Projetos</h2>
-        <Link
-          href='/projects'
-          className='rounded bg-blue-600 py-2 px-3 transition-colors hover:bg-violet-700 focus-visible:outline focus:outline-blue-500 focus:outline-2 focus:outline-offset-2'
-        >
-          Ver todos
-        </Link>
+        <LinkButton href='/projects'>Ver todos</LinkButton>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
         {numbers.map((n, index) => {
