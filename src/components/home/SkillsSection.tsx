4ccd14304f67d426edd581bd9c25fdddd994f1b2
Adriel <adriel.fsantos@outlook.com>
2023-07-22 23:09:31 -0300

    Insere HelperComponent em SkillsSection

--- src/components/home/SkillsSection.tsx
+++ src/components/home/SkillsSection.tsx
@@ -9,6 +9,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Modal from '../Modal';
 import SkillCard from '../SkillCard';
 import SkillModal from '../SkillModal';
+import HelperComponent from '../HelperComponent';
 
 export default function SkillsSection() {
   const dispatch = useDispatch<AppDispatch>();
@@ -25,14 +26,29 @@ export default function SkillsSection() {
   return (
     <section className='container mx-auto min-h-screen flex flex-col justify-center p-5 gap-2'>
       <h2 className='text-4xl font-semibold mb-3'>Habilidades</h2>
-      <Modal open={showModal} onOpenChange={setShowModal}>
-        <div className='grid grid-cols-3 md:grid-cols-6 lg:grid-cols-12 gap-3'>
-          {skills.map((skill) => (
-            <SkillCard skill={skill} delay={Math.random() * 5} key={skill.id} />
-          ))}
-        </div>
-        <SkillModal open={showModal} />
-      </Modal>
+      {status === 'succeeded' ? (
+        skills.length ? (
+          <Modal open={showModal} onOpenChange={setShowModal}>
+            <div className='grid grid-cols-3 md:grid-cols-6 lg:grid-cols-12 gap-3'>
+              {skills.map((skill) => (
+                <SkillCard
+                  skill={skill}
+                  delay={Math.random() * 5}
+                  key={skill.id}
+                />
+              ))}
+            </div>
+            <SkillModal open={showModal} />
+          </Modal>
+        ) : (
+          <HelperComponent
+            option='noElements'
+            noElementsMessage='Nenhuma habilidade encontrada'
+          />
+        )
+      ) : (
+        <HelperComponent option={status} />
+      )}
     </section>
   );
 }
