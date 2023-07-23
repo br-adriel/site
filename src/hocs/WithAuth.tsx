853eb8188989dba20f5a26d1080e97df6480b917
Adriel <adriel.fsantos@outlook.com>
2023-07-23 16:53:50 -0300

    Adiciona HOC para páginas que precisam de autenticação

---
+++ src/hocs/WithAuth.tsx
@@ -0,0 +1,20 @@
+import LoadingScreen from '@/components/LoadingScreen';
+import { selectUser } from '@/store/authSlice';
+import { useRouter } from 'next/router';
+import { ComponentType, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+export default function WithAuth(Component: ComponentType<any>) {
+  return function (props: any) {
+    const router = useRouter();
+    const user = useSelector(selectUser);
+
+    useEffect(() => {
+      if (!user) {
+        router.push('/admin/login');
+      }
+    }, [user, router]);
+
+    return user ? <Component {...props} /> : <LoadingScreen />;
+  };
+}
