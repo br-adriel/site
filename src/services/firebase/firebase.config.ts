09386873891e883bbde78e3cf6838f2197a6f751
Adriel <adriel.fsantos@outlook.com>
2023-07-23 16:50:57 -0300

    Adiciona export do módulo de autenticação do firebase

--- src/services/firebase/firebase.config.ts
+++ src/services/firebase/firebase.config.ts
@@ -1,5 +1,6 @@
-import { getFirestore } from '@firebase/firestore';
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_apiKey,
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
