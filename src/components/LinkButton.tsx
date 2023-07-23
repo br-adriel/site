3b7de43951e20b4d2e15fc2a7139254cd0348182
Adriel <adriel.fsantos@outlook.com>
2023-07-23 00:55:01 -0300

    Corrige cor de texto de LinkButton

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -9,7 +9,7 @@ export default function LinkButton({ href, children }: IProps) {
   return (
     <Link
       href={href}
-      className='rounded bg-blue-600 py-2 px-3 transition-colors hover:bg-violet-700 focus-visible:outline focus:outline-blue-500 focus:outline-2 focus:outline-offset-2'
+      className='rounded bg-blue-600 py-2 px-3 transition-colors hover:bg-violet-700 focus-visible:outline focus:outline-blue-500 focus:outline-2 focus:outline-offset-2 text-white'
     >
       {children}
     </Link>
