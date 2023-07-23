9766446e294dd02998af903dfcb62427986499be
Adriel <adriel.fsantos@outlook.com>
2023-07-23 12:13:44 -0300

    COrrige função getShortMonthName

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,7 +2,7 @@ export function getShortMonthName(
   month: number,
   locale: string = 'pt-BR'
 ): string {
-  const monthName = new Date(`2020-${month}-01`)
+  const monthName = new Date(`2020-${month}-15`)
     .toLocaleString(locale, { month: 'short' })
     .split('');
   monthName[0] = monthName[0].toLocaleUpperCase();
