2c737387bdcf3f7cc7a3a17ab9c3f5d78c1a4a48
Adriel <adriel.fsantos@outlook.com>
2023-07-22 21:55:49 -0300

    Adiciona função para retorno de mês do ano

---
+++ src/utils/date.ts
@@ -0,0 +1,10 @@
+export function getShortMonthName(
+  month: number,
+  locale: string = 'pt-BR'
+): string {
+  const monthName = new Date(`2020-${month}-01`)
+    .toLocaleString(locale, { month: 'short' })
+    .split('');
+  monthName[0] = monthName[0].toLocaleUpperCase();
+  return monthName.join('');
+}
