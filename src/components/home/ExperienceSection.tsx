b24c16439bd7ce79b1c00990b75ca5e03594b2b9
Adriel <adriel.fsantos@outlook.com>
2023-07-22 23:00:49 -0300

    Insere HelperComponent em ExperienceSection

--- src/components/home/ExperienceSection.tsx
+++ src/components/home/ExperienceSection.tsx
@@ -7,6 +7,7 @@ import {
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ExperienceCard from '../ExperienceCard';
+import HelperComponent from '../HelperComponent';
 
 export default function ExperienceSection() {
   const dispatch = useDispatch<AppDispatch>();
@@ -21,11 +22,25 @@ export default function ExperienceSection() {
   return (
     <section className='container mx-auto min-h-screen flex flex-col justify-center p-5 gap-2'>
       <h2 className='text-4xl font-semibold mb-3'>Experiência</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
-        {experiences.map((exp, index) => {
-          return <ExperienceCard experience={exp} delay={index} key={exp.id} />;
-        })}
-      </div>
+
+      {status === 'succeeded' ? (
+        experiences.length ? (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+            {experiences.map((exp, index) => {
+              return (
+                <ExperienceCard experience={exp} delay={index} key={exp.id} />
+              );
+            })}
+          </div>
+        ) : (
+          <HelperComponent
+            option='noElements'
+            noElementsMessage='Nenhuma experiência encontrada'
+          />
+        )
+      ) : (
+        <HelperComponent option={status} />
+      )}
     </section>
   );
 }
