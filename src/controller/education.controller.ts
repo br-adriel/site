230ad285bc3c8c7cec84033ae620406315671fcd
Adriel <adriel.fsantos@outlook.com>
2023-07-24 15:02:29 -0300

    Adiciona função para adicionar educação

--- src/controller/education.controller.ts
+++ src/controller/education.controller.ts
@@ -1,17 +1,23 @@
 import IEducation from '@/interfaces/IEducation';
 import { db } from '@/services/firebase/firebase.config';
 import { getQuery } from '@/services/firebase/utils';
-import { collection, orderBy, query } from '@firebase/firestore';
+import { addDoc, collection, orderBy, query } from '@firebase/firestore';
 
 export default class EducationController {
+  private static collectionRef = collection(db, 'educacao');
+
   static async getAll() {
-    const educationCollectionRef = collection(db, 'educacao');
     const q = query(
-      educationCollectionRef,
+      EducationController.collectionRef,
       orderBy('anoInicio', 'desc'),
       orderBy('mesInicio', 'desc')
     );
     const fetchedEducation = await getQuery(q);
     return fetchedEducation as IEducation[];
+  }
+
+  static async add(education: Omit<IEducation, 'id'>): Promise<IEducation> {
+    const docRef = await addDoc(EducationController.collectionRef, education);
+    return { ...education, id: docRef.id };
   }
 }
