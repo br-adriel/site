4e0994eeaeaa2c8de64b9a4555872de3d50f3384
Adriel <adriel.fsantos@outlook.com>
2023-07-24 20:53:16 -0300

    Adiciona função para adição de experiencia

--- src/controller/experience.controller.ts
+++ src/controller/experience.controller.ts
@@ -1,17 +1,23 @@
 import IExperience from '@/interfaces/IExperience';
 import { db } from '@/services/firebase/firebase.config';
 import { getQuery } from '@/services/firebase/utils';
-import { collection, orderBy, query } from '@firebase/firestore';
+import { addDoc, collection, orderBy, query } from '@firebase/firestore';
 
 export default class ExperienceController {
+  private static collectionRef = collection(db, 'experiencia');
+
   static async getAll() {
-    const experiencesCollectionRef = collection(db, 'experiencia');
     const q = query(
-      experiencesCollectionRef,
+      ExperienceController.collectionRef,
       orderBy('anoInicio', 'desc'),
       orderBy('mesInicio', 'desc')
     );
     const fetchedExperiences = await getQuery(q);
     return fetchedExperiences as IExperience[];
+  }
+
+  static async add(experience: Omit<IExperience, 'id'>): Promise<IExperience> {
+    const docRef = await addDoc(ExperienceController.collectionRef, experience);
+    return { ...experience, id: docRef.id };
   }
 }
