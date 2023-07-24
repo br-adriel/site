2f897e5416884561f384f9084926779a1956558e
Adriel <adriel.fsantos@outlook.com>
2023-07-24 20:38:08 -0300

    Atualiza EducationCard com botões de ação

--- src/components/EducationCard.tsx
+++ src/components/EducationCard.tsx
@@ -2,13 +2,22 @@ import { fadeInUpAnimation } from '@/animations/FadeInUp';
 import IEducation from '@/interfaces/IEducation';
 import { getShortMonthName } from '@/utils/date';
 import { motion } from 'framer-motion';
+import { Pen, Trash } from 'react-bootstrap-icons';
+import Button from './Button';
 
 interface IProps {
   education: IEducation;
   delay?: number;
+  onEdit?: (id: string) => void;
+  onRemove?: (id: string) => void;
 }
 
-export default function EducationCard({ education, delay }: IProps) {
+export default function EducationCard({
+  education,
+  delay,
+  onEdit,
+  onRemove,
+}: IProps) {
   const animation = fadeInUpAnimation(0.2 * (delay || 0));
 
   const startMonthName: string = getShortMonthName(education.mesInicio);
@@ -30,6 +39,24 @@ export default function EducationCard({ education, delay }: IProps) {
         {education.curso} ({education.instituicao})
       </h3>
       <p className='text-md opacity-70'>{startDate + ' - ' + endDate}</p>
+      {onEdit && onRemove ? (
+        <div className='mt-2 flex gap-2'>
+          <Button
+            type='button'
+            title='Editar'
+            onClick={() => onEdit(education.id)}
+          >
+            <Pen />
+          </Button>
+          <Button
+            type='button'
+            title='Remover'
+            onClick={() => onRemove(education.id)}
+          >
+            <Trash />
+          </Button>
+        </div>
+      ) : null}
     </motion.div>
   );
 }
