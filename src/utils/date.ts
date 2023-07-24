f1d9447bd27f3b978d99c8a8605b24fd23896801
Adriel <adriel.fsantos@outlook.com>
2023-07-24 17:07:54 -0300

    Adiciona objeto de opções para select de mês

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -8,3 +8,18 @@ export function getShortMonthName(
   monthName[0] = monthName[0].toLocaleUpperCase();
   return monthName.join('');
 }
+
+export const monthSelectOptionsPtBr = [
+  { text: 'Janeiro', value: 1 },
+  { text: 'Fevereiro', value: 2 },
+  { text: 'Março', value: 3 },
+  { text: 'Abril', value: 4 },
+  { text: 'Maio', value: 5 },
+  { text: 'Junho', value: 6 },
+  { text: 'Julho', value: 7 },
+  { text: 'Agosto', value: 8 },
+  { text: 'Setembro', value: 9 },
+  { text: 'Outubro', value: 10 },
+  { text: 'Novembro', value: 11 },
+  { text: 'Dezembro', value: 12 },
+];
