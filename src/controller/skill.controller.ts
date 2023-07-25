9e86c0da4b9d760dbaf247ad526826ab53bd657d
Adriel <adriel.fsantos@outlook.com>
2023-07-24 21:03:26 -0300

    Adiciona função para atualizar habilidade

--- src/controller/skill.controller.ts
+++ src/controller/skill.controller.ts
@@ -1,7 +1,14 @@
 import ISkill from '@/interfaces/ISkill';
 import { db } from '@/services/firebase/firebase.config';
 import { getQuery } from '@/services/firebase/utils';
-import { addDoc, collection, orderBy, query } from '@firebase/firestore';
+import {
+  addDoc,
+  collection,
+  doc,
+  orderBy,
+  query,
+  updateDoc,
+} from '@firebase/firestore';
 
 export default class SkillController {
   private static collectionRef = collection(db, 'habilidades');
@@ -15,5 +22,11 @@ export default class SkillController {
   static async add(skill: Omit<ISkill, 'id'>): Promise<ISkill> {
     const docRef = await addDoc(this.collectionRef, skill);
     return { ...skill, id: docRef.id };
+  }
+
+  static async update(skill: Omit<ISkill, 'id'>, id: string): Promise<ISkill> {
+    const docRef = doc(db, 'habilidades', id);
+    await updateDoc(docRef, skill);
+    return { ...skill, id };
   }
 }
