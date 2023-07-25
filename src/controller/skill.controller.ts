b87b09874f156cd1204b7ebf75b1523a5a3cc5b1
Adriel <adriel.fsantos@outlook.com>
2023-07-24 21:00:06 -0300

    Adiciona função para adição de habilidade

--- src/controller/skill.controller.ts
+++ src/controller/skill.controller.ts
@@ -1,13 +1,19 @@
 import ISkill from '@/interfaces/ISkill';
 import { db } from '@/services/firebase/firebase.config';
 import { getQuery } from '@/services/firebase/utils';
-import { collection, orderBy, query } from '@firebase/firestore';
+import { addDoc, collection, orderBy, query } from '@firebase/firestore';
 
 export default class SkillController {
+  private static collectionRef = collection(db, 'habilidades');
+
   static async getAll() {
-    const skillsCollectionRef = collection(db, 'habilidades');
-    const q = query(skillsCollectionRef, orderBy('ordem', 'asc'));
+    const q = query(this.collectionRef, orderBy('ordem', 'asc'));
     const fetchedSkills = await getQuery(q);
     return fetchedSkills as ISkill[];
+  }
+
+  static async add(skill: Omit<ISkill, 'id'>): Promise<ISkill> {
+    const docRef = await addDoc(this.collectionRef, skill);
+    return { ...skill, id: docRef.id };
   }
 }
