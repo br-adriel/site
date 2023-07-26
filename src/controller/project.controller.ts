c9b4190fab9dd3598669913d0dce8818de9e3c3e
Adriel <adriel.fsantos@outlook.com>
2023-07-26 20:29:33 -0300

    Adiciona método para criar projeto

--- src/controller/project.controller.ts
+++ src/controller/project.controller.ts
@@ -1,7 +1,7 @@
 import IProject from '@/interfaces/IProject';
 import { db } from '@/services/firebase/firebase.config';
 import { getQuery } from '@/services/firebase/utils';
-import { collection, orderBy, query } from '@firebase/firestore';
+import { addDoc, collection, orderBy, query } from '@firebase/firestore';
 
 export default class ProjectController {
   private static collectionRef = collection(db, 'projetos');
@@ -10,5 +10,10 @@ export default class ProjectController {
     const q = query(this.collectionRef, orderBy('dataCriacao', 'desc'));
     const fetchedProjects = await getQuery(q);
     return fetchedProjects as IProject[];
+  }
+
+  static async add(project: Omit<IProject, 'id'>): Promise<IProject> {
+    const docRef = await addDoc(this.collectionRef, project);
+    return { ...project, id: docRef.id };
   }
 }
