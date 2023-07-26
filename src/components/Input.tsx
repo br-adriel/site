6ad21ef30416f74d9ce4334941f5499a9f6181b7
Adriel <adriel.fsantos@outlook.com>
2023-07-25 21:17:21 -0300

    Corrige atribuição de classe em Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,7 +11,7 @@ export default function Input(props: IProps) {
     <input
       {...props}
       className={`text-black p-1 rounded focus:outline-2 focus-within:outline-none focus:outline-blue-600 focus:outline-offset-2 caret-blue-600 ${
-        props.className || 'first-letter:'
+        props.className || ''
       }`}
     />
   );
