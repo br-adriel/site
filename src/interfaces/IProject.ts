287a068f3b43c790b0962907ebdbf177eabac965
Adriel <adriel.fsantos@outlook.com>
2023-07-26 20:38:11 -0300

    Atualiza interface IProject

--- src/interfaces/IProject.ts
+++ src/interfaces/IProject.ts
@@ -1,10 +1,10 @@
 export default interface IProject {
-  id: string;
-  nome: string;
-  imagem: string;
-  descricao: string;
   dataCriacao: Date;
-  linkVisualizacao: string;
-  linkRepositorio: string;
+  descricao: string;
+  id: string;
+  imagem: string;
+  nome: string;
   tecnologias: string[];
+  linkRepositorio?: string;
+  linkVisualizacao?: string;
 }
