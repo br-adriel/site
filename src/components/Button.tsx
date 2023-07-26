f61cf6b033afebcdf2218c1f99495746f3b20547
Adriel <adriel.fsantos@outlook.com>
2023-07-25 21:17:05 -0300

    Corrige cor de foco do Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface IProps
 export default function Button(props: IProps) {
   return (
     <button
-      className='rounded bg-blue-600 py-2 px-3 transition-colors hover:bg-violet-700 focus-visible:outline focus:outline-blue-500 focus:outline-2 focus:outline-offset-2 text-white text-center flex items-center justify-center'
+      className='rounded bg-blue-600 py-2 px-3 transition-colors hover:bg-violet-700 focus-visible:outline focus:outline-blue-600 focus:outline-2 focus:outline-offset-2 text-white text-center flex items-center justify-center'
       {...props}
     >
       {props.children}
