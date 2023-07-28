2e648b5e464b4024d6794063ed09ebb9aa0a2909
Adriel <adriel.fsantos@outlook.com>
2023-07-27 22:04:47 -0300

    Adiciona função joinDocDataAndId

--- src/services/firebase/utils.ts
+++ src/services/firebase/utils.ts
@@ -1,14 +1,20 @@
-import { DocumentData, Query, getDocs } from 'firebase/firestore';
+import {
+  DocumentData,
+  DocumentSnapshot,
+  Query,
+  getDocs,
+} from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { storage } from './firebase.config';
 
 export async function getQuery(query: Query<DocumentData>) {
   const data = await getDocs(query);
-  const fetchedObjects = data.docs.map((doc) => ({
-    ...doc.data(),
-    id: doc.id,
-  }));
+  const fetchedObjects = data.docs.map((doc) => joinDocDataAndId(doc));
   return fetchedObjects;
+}
+
+export function joinDocDataAndId(doc: DocumentSnapshot) {
+  return { ...doc.data(), id: doc.id };
 }
 
 export async function uploadFile(dest: string, file: File) {
