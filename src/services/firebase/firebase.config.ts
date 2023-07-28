95c0fe6bf6280315d8427fe903447fd872d0785b
Adriel <adriel.fsantos@outlook.com>
2023-07-27 22:07:43 -0300

    Exporta objeto storage

--- src/services/firebase/firebase.config.ts
+++ src/services/firebase/firebase.config.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_apiKey,
@@ -15,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
