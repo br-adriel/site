f211af6134e8371988088ed3c4b8f6ac24ae2f2a
Adriel <adriel.fsantos@outlook.com>
2023-07-27 22:03:52 -0300

    Adiciona função uploadFile

--- src/services/firebase/utils.ts
+++ src/services/firebase/utils.ts
@@ -1,4 +1,6 @@
 import { DocumentData, Query, getDocs } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { storage } from './firebase.config';
 
 export async function getQuery(query: Query<DocumentData>) {
   const data = await getDocs(query);
@@ -7,4 +9,11 @@ export async function getQuery(query: Query<DocumentData>) {
     id: doc.id,
   }));
   return fetchedObjects;
+}
+
+export async function uploadFile(dest: string, file: File) {
+  const fileRef = ref(storage, dest);
+  const uploaded = await uploadBytes(fileRef, file);
+  const url = await getDownloadURL(uploaded.ref);
+  return url;
 }
