45db774240a46d5b0b8e05ca63da8191e8ab15ff
Adriel <adriel.fsantos@outlook.com>
2023-07-28 21:41:30 -0300

    Implementa exibição dinâmica do modal de habilidades

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,8 +1,10 @@
 import { fadeInGrow } from '@/animations/FadeInGrow';
 import ISkill from '@/interfaces/ISkill';
+import { setSelectedSkill } from '@/store/skillsSlice';
 import { Trigger } from '@radix-ui/react-dialog';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { useDispatch } from 'react-redux';
 
 interface IProps {
   delay?: number;
@@ -11,11 +13,13 @@ interface IProps {
 
 export default function SkillCard({ delay, skill }: IProps) {
   const cardAnimation = fadeInGrow(0.1 * (delay || 0), 0.2);
+  const dispatch = useDispatch();
 
   return (
     <motion.div {...cardAnimation}>
       <Trigger
         title={skill.nome}
+        onClick={() => dispatch(setSelectedSkill(skill))}
         className='bg-alt_bg p-2 rounded flex items-center justify-center cursor-pointer shadow hover:shadow-md focus:outline-blue-600 focus:outline-2 focus:outline-offset-4 hover:scale-105 transition-all'
       >
         <Image
--- src/components/SkillModal.tsx
+++ src/components/SkillModal.tsx
@@ -1,4 +1,7 @@
+import { selectSelectedSkill } from '@/store/skillsSlice';
 import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import LoadingScreen from './LoadingScreen';
 import ModalBody from './Modal/ModalBody';
 
 interface IProps {
@@ -6,28 +9,36 @@ interface IProps {
 }
 
 export default function SkillModal({ open }: IProps) {
+  const skill = useSelector(selectSelectedSkill);
+
   return (
-    <ModalBody open={open} title={<h2 className='font-semibold'>HTML</h2>}>
-      <div className='w-full lg:w-1/3 xl:w-1/4 flex flex-col sm:flex-row lg:flex-col gap-3'>
-        <Image
-          width={200}
-          height={200}
-          alt=''
-          src='https://www.svgrepo.com/show/349402/html5.svg'
-          className='w-full sm:w-2/6 lg:w-full p-10 bg-white rounded mb-3 inline-block'
-        />
-        <p className=''>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ullam
-          tempora molestias consectetur nihil in nobis, eum, pariatur beatae, id
-          sunt aut blanditiis rerum. Dolorum, eos a quam architecto enim
-          dolorum. Consectetur, quam nam.
-        </p>
-      </div>
-      <div className='w-full lg:w-2/3 xl:w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2'>
-        {/* <ProjectCard />
+    <ModalBody
+      open={open}
+      title={
+        <h2 className='font-bold'>{skill ? skill.nome : 'Carregando...'}</h2>
+      }
+    >
+      {skill ? (
+        <>
+          <div className='w-full lg:w-1/3 xl:w-1/4 flex flex-col sm:flex-row lg:flex-col gap-3'>
+            <Image
+              width={200}
+              height={200}
+              alt=''
+              src={skill.imagem}
+              className='w-full sm:w-2/6 lg:w-full p-10 bg-white rounded mb-3 inline-block'
+            />
+            <p>{skill.descricao}</p>
+          </div>
+          <div className='w-full lg:w-2/3 xl:w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2'>
+            {/* <ProjectCard />
         <ProjectCard />
-        <ProjectCard /> */}
-      </div>
+      <ProjectCard /> */}
+          </div>
+        </>
+      ) : (
+        <LoadingScreen />
+      )}
     </ModalBody>
   );
 }
