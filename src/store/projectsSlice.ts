4bb9af17ea5f5430f6313c7a89ca48e78ddc4521
Adriel <adriel.fsantos@outlook.com>
2023-07-29 00:26:04 -0300

    Melhora formatação de projectSlice

--- src/store/projectsSlice.ts
+++ src/store/projectsSlice.ts
@@ -5,14 +5,13 @@ import { RootState } from '.';
 
 type StateType = {
   data: IProject[];
-  status: 'idle' | 'loading' | 'succeeded' | 'failed';
   formStatus: 'idle' | 'loading' | 'succeeded' | 'failed';
   formValues: IProject;
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
 };
 
 const initialState: StateType = {
   data: [],
-  status: 'idle',
   formStatus: 'idle',
   formValues: {
     id: '',
@@ -24,6 +23,7 @@ const initialState: StateType = {
     nome: '',
     tecnologias: [],
   },
+  status: 'idle',
 };
 
 /**
@@ -174,8 +174,11 @@ export const projectsReducer = experiencesSlice.reducer;
 
 // Seletores
 export const selectAllProjects = (state: RootState) => state.projects.data;
+
 export const selectProjectsStatus = (state: RootState) => state.projects.status;
+
 export const selectProjectsFormStatus = (state: RootState) =>
   state.projects.formStatus;
+
 export const selectProjectsFormValues = (state: RootState) =>
   state.projects.formValues;
