34b677be955cb9de05f67e5ea728d20b0107a1d2
Adriel <adriel.fsantos@outlook.com>
2023-07-28 21:41:01 -0300

    Corrige erros de Modal

--- src/components/Modal/ModalBackground.tsx
+++ src/components/Modal/ModalBackground.tsx
@@ -1,7 +1,11 @@
-export default function ModalBackground() {
+import { forwardRef } from 'react';
+
+function ModalBackground() {
   return (
     <div className='fixed top-0 left-0 w-screen h-screen z-0'>
       <div className='w-full h-full bg-blue-950 opacity-20' />
     </div>
   );
 }
+
+export default forwardRef(ModalBackground);
