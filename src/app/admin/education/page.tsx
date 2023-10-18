23d21c385301a068e0ff8aa195cd0c29f547ea3b
Adriel <adriel.fsantos@outlook.com>
2023-10-18 20:13:01 -0300

    Adapta página de adm de educação a pasta app

--- src/app/admin/education/page.tsx
+++ src/app/admin/education/page.tsx
@@ -1,45 +1,18 @@
-'use client';
-
-import EducationCard from '@/components/EducationCard';
-import HelperComponent from '@/components/HelperComponent';
 import EducationForm from '@/components/admin/EducationForm';
+import EducationList from '@/components/admin/EducationList';
 import Header from '@/components/admin/Header';
 import WithAuth from '@/hocs/WithAuth';
-import { AppDispatch } from '@/store';
-import {
-  fetchEducation,
-  removeEducation,
-  selectAllEducation,
-  selectEducationStatus,
-  setFormvalues,
-} from '@/store/educationSlice';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { Metadata } from 'next';
 
-function Education() {
-  const dispatch = useDispatch<AppDispatch>();
-  const listStatus = useSelector(selectEducationStatus);
-  const education = useSelector(selectAllEducation);
+export const metadata: Metadata = {
+  title: 'Seção Educação | Adminitração do Site',
+  description: 'Visualize e edite as educações exibidas no site',
+};
 
-  const editEducation = (id: string) => {
-    const selectedEducation = education.filter((ed) => ed.id === id)[0];
-    dispatch(setFormvalues(selectedEducation));
-  };
-
-  useEffect(() => {
-    if (listStatus === 'idle') dispatch(fetchEducation());
-  }, [dispatch, listStatus]);
-
+export default function Education() {
   return (
-    <>
-      {/* <Head>
-        <title>Seção Educação | Adminitração do Site</title>
-        <meta
-          name='description'
-          content='Visualize e edite as educações exibidas no site'
-        />
-      </Head> */}
-
+    <WithAuth>
+      {' '}
       <Header />
       <main className='container mx-auto p-3'>
         <h2 className='text-2xl font-semibold mb-3'>Educação</h2>
@@ -48,32 +21,10 @@ function Education() {
             <EducationForm />
           </div>
           <div className='col-span-12 md:col-span-7 lg:col-span-9'>
-            {listStatus === 'succeeded' ? (
-              <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
-                {education.length ? (
-                  education.map((ed) => (
-                    <EducationCard
-                      education={ed}
-                      key={ed.id}
-                      onEdit={editEducation}
-                      onRemove={(id: string) => dispatch(removeEducation(id))}
-                    />
-                  ))
-                ) : (
-                  <HelperComponent
-                    option='noElements'
-                    noElementsMessage='Nenhum registro encontrado'
-                  />
-                )}
-              </div>
-            ) : (
-              <HelperComponent option={listStatus} />
-            )}
+            <EducationList />
           </div>
         </div>
       </main>
-    </>
+    </WithAuth>
   );
 }
-
-export default WithAuth(Education);
