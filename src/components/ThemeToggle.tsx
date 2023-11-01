11daba72869502eacbc0c5247018bf8413ec18f9
Adriel <adriel.fsantos@outlook.com>
2023-11-01 14:35:42 -0300

    Internacionalize ThemeToggle #18

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,9 +2,11 @@
 
 import { getThemePreference } from '@/contexts/ThemeContext';
 import * as RadixSwitch from '@radix-ui/react-switch';
+import { useTranslations } from 'next-intl';
 import { Moon, Sun } from 'react-bootstrap-icons';
 
 export default function ThemeToggle() {
+  const t = useTranslations('home.page');
   const { toggleTheme, usingDarkTheme } = getThemePreference();
 
   return (
@@ -16,7 +18,7 @@ export default function ThemeToggle() {
       >
         <RadixSwitch.Thumb
           className='bg-siteBg-light dark:bg-siteBg-dark w-6 h-6 rounded absolute top-0.5 left-0.5 data-[state=checked]:left-auto data-[state=checked]:translate-x-3.5 transition-transform flex items-center justify-center shadow-sm'
-          title='Alterar tema'
+          title={t('theme-button-text')}
         >
           {usingDarkTheme ? <Moon /> : <Sun />}
         </RadixSwitch.Thumb>
