22aa74ada13572744d16e3677f57c0d144b834c0
Adriel <adriel.fsantos@outlook.com>
2023-11-01 15:56:10 -0300

    Internacionalize EducationSection and its components #18

--- src/components/EducationCard.tsx
+++ src/components/EducationCard.tsx
@@ -4,6 +4,7 @@ import { fadeInUpAnimation } from '@/animations/FadeInUp';
 import IEducation from '@/interfaces/IEducation';
 import { getShortMonthName } from '@/utils/date';
 import { motion } from 'framer-motion';
+import { useParams } from 'next/navigation';
 import { Pen, Trash } from 'react-bootstrap-icons';
 import Button from './Button';
 
@@ -20,11 +21,13 @@ export default function EducationCard({
   onEdit,
   onRemove,
 }: IProps) {
+  const { locale } = useParams<{ locale: string }>();
+
   const animation = fadeInUpAnimation(0.2 * (delay || 0));
 
-  const startMonthName: string = getShortMonthName(education.mesInicio);
+  const startMonthName: string = getShortMonthName(education.mesInicio, locale);
   const endMonthName: string = education.mesFim
-    ? getShortMonthName(education.mesFim)
+    ? getShortMonthName(education.mesFim, locale)
     : '';
 
   const startDate: string = `${startMonthName} ${education.anoInicio}`;
--- src/components/home/EducationSection.tsx
+++ src/components/home/EducationSection.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import IEducation from '@/interfaces/IEducation';
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import EducationCard from '../EducationCard';
 import HelperComponent from '../HelperComponent';
@@ -8,11 +11,14 @@ interface IProps {
 }
 
 export default function EducationSection({ education }: IProps) {
+  const t = useTranslations('home.page');
   return (
     <section className='container p-5 min-h-screen mx-auto flex flex-col md:flex-row-reverse items-center justify-center gap-12 lg:gap-5'>
       <div className='w-full py-5 md:w-1/2 lg:pl-9 xl:pl-9'>
         <div className='flex flex-col gap-2'>
-          <h2 className='text-4xl font-semibold mb-3'>Educação</h2>
+          <h2 className='text-4xl font-semibold mb-3'>
+            {t('education-title')}
+          </h2>
 
           {education.length ? (
             education.map((ed, index) => {
@@ -23,7 +29,7 @@ export default function EducationSection({ education }: IProps) {
           ) : (
             <HelperComponent
               option='noElements'
-              noElementsMessage='Nenhuma educação encontrada'
+              noElementsMessage={t('no-education')}
             />
           )}
         </div>
