394fc074b3babb1908b289d0e3dd74cbf057a624
Adriel <adriel.fsantos@outlook.com>
2023-11-01 15:07:39 -0300

    Internacionalize SkillSection and components #18

--- src/components/home/SkillsSection.tsx
+++ src/components/home/SkillsSection.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import ISkill from '@/interfaces/ISkill';
+import { useTranslations } from 'next-intl';
 import { useState } from 'react';
 import HelperComponent from '../HelperComponent';
 import Modal from '../Modal';
@@ -12,11 +13,13 @@ interface IProps {
 }
 
 export default function SkillsSection({ skills }: IProps) {
+  const t = useTranslations('home.page');
+
   const [showModal, setShowModal] = useState<boolean>(false);
 
   return (
     <section className='container mx-auto min-h-screen flex flex-col justify-center p-5 gap-2'>
-      <h2 className='text-4xl font-semibold mb-3'>Habilidades</h2>
+      <h2 className='text-4xl font-semibold mb-3'>{t('skills-title')}</h2>
       {skills.length ? (
         <Modal open={showModal} onOpenChange={setShowModal}>
           <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-12 gap-3'>
@@ -33,7 +36,7 @@ export default function SkillsSection({ skills }: IProps) {
       ) : (
         <HelperComponent
           option='noElements'
-          noElementsMessage='Nenhuma habilidade encontrada'
+          noElementsMessage={t('no-skills')}
         />
       )}
     </section>
