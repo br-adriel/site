951c51f08fc82b06b85f233cde97fa394d7a9f43
Adriel <adriel.fsantos@outlook.com>
2023-11-01 14:41:53 -0300

    Internacionalize ExperienceSection #18

--- src/components/home/ExperienceSection.tsx
+++ src/components/home/ExperienceSection.tsx
@@ -1,15 +1,20 @@
+'use client';
+
 import IExperience from '@/interfaces/IExperience';
 import ExperienceCard from '../ExperienceCard';
 import HelperComponent from '../HelperComponent';
+import { useTranslations } from 'next-intl';
 
 interface IProps {
   experiences: IExperience[];
 }
 
 export default function ExperienceSection({ experiences }: IProps) {
+  const t = useTranslations('home.page');
+
   return (
     <section className='container mx-auto min-h-screen flex flex-col justify-center p-5 gap-2'>
-      <h2 className='text-4xl font-semibold mb-3'>Experiência</h2>
+      <h2 className='text-4xl font-semibold mb-3'>{t('experience-title')}</h2>
 
       {experiences.length ? (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
@@ -22,7 +27,7 @@ export default function ExperienceSection({ experiences }: IProps) {
       ) : (
         <HelperComponent
           option='noElements'
-          noElementsMessage='Nenhuma experiência encontrada'
+          noElementsMessage={t('no-experiences')}
         />
       )}
     </section>
