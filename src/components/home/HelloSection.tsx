ac15a98f586bbf372c58de5df9afd5fd91014628
Adriel <adriel.fsantos@outlook.com>
2023-11-01 14:34:20 -0300

    Internacionalize HelloSection #18

--- src/components/home/HelloSection.tsx
+++ src/components/home/HelloSection.tsx
@@ -2,6 +2,7 @@
 
 import { fadeInAnimation } from '@/animations/FadeIn';
 import { motion } from 'framer-motion';
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import { Github, Icon, Linkedin } from 'react-bootstrap-icons';
 import SocialLink from '../SocialLink';
@@ -15,6 +16,8 @@ interface ILinkSocial {
 }
 
 export default function HelloSection() {
+  const t = useTranslations('home.page');
+
   const linksSociais: ILinkSocial[] = [
     {
       icon: Github,
@@ -47,7 +50,7 @@ export default function HelloSection() {
           {...h2Animation}
           className='text-center md:text-left text-2xl lg:text-3xl capitalize'
         >
-          Desenvolvedor fullstack
+          {t('title')}
         </motion.p>
 
         <div className='flex justify-center md:justify-start items-center gap-2 mt-4'>
