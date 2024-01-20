4807348c1c2eb7fd1caab571ceabe5e59ad8f11b
Adriel <adriel.fsantos@outlook.com>
2024-01-20 20:19:48 -0300

    feat: Improve LinkButton typing and look #22

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import { PropsWithChildren } from 'react';
 
-interface IProps extends PropsWithChildren {
-  href: string;
-}
+interface IProps extends LinkProps, PropsWithChildren {}
 
-export default function LinkButton({ href, children }: IProps) {
+export default function LinkButton({ children, ...attibutes }: IProps) {
   return (
     <Link
-      href={href}
-      className='rounded bg-blue-600 py-2 px-3 transition-colors hover:bg-violet-700 focus-visible:outline focus:outline-blue-500 focus:outline-2 focus:outline-offset-2 text-white'
+      {...attibutes}
+      className='rounded bg-blue-600 py-2 px-3 transition-colors hover:bg-violet-700 focus-visible:outline focus:outline-blue-500 focus:outline-2 focus:outline-offset-2 text-white flex justify-center items-center'
     >
       {children}
     </Link>
